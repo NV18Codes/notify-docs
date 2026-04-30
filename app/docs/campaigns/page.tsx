0bd69d2dc8579ff47061dd8f530d9d89693b1f79
NavyaVenugopal <you@example.com>
2026-04-30 17:59:38 +0530

    added images for carousel templates and campaign module added

--- app/docs/campaigns/page.tsx
+++
@@ -1,6 +0,0 @@
-import { PlaceholderDoc } from "@/components/docs/PlaceholderDoc";
-import { copy } from "@/lib/copy";
-
-export default function CampaignsPage() {
-  return <PlaceholderDoc title={copy.nav.campaigns} />;
-}
